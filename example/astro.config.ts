34d3e82b72eba312644d7967ba10dd87269cc75e
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-07-28 16:32:23 +0200

    chore: setup monorepo

---
+++ example/astro.config.ts
@@ -0,0 +1,18 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      editLink: {
+        baseUrl: 'https://github.com/HiDeoo/starlight-package-managers/edit/main/example/',
+      },
+      sidebar: [{ label: 'Getting Started', link: '/guides/getting-started/' }],
+      social: {
+        github: 'https://github.com/HiDeoo/starlight-package-managers',
+      },
+      title: 'Starlight Package Managers',
+    }),
+  ],
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+})
