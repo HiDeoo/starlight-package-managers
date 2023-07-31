e864da91fdd23d0706f937e32f5feb2c31ba3c8c
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-07-31 17:54:08 +0200

    feat: add dev dependency command

--- packages/starlight-package-managers/pkg.ts
+++ packages/starlight-package-managers/pkg.ts
@@ -1,27 +1,44 @@
 export const pkgManagers = ['npm', 'yarn', 'pnpm', 'bun', 'ni'] as const
 
-const commands: Record<PackageManager, Record<CommandType, string>> = {
+const commands: Record<PackageManager, Record<CommandType | 'devOption', string>> = {
   npm: {
     add: 'npm i',
+    devOption: '-D',
   },
   yarn: {
     add: 'yarn add',
+    devOption: '-D',
   },
   pnpm: {
     add: 'pnpm add',
+    devOption: '-D',
   },
   bun: {
     add: 'bun add',
+    devOption: '-d',
   },
   ni: {
     add: 'ni',
+    devOption: '-D',
   },
 }
 
-export function getCommand(pkgManager: PackageManager, type: CommandType, pkg: string) {
-  return `${commands[pkgManager][type]} ${pkg}`
+export function getCommand(pkgManager: PackageManager, type: CommandType, pkg: string, options: CommandOptions) {
+  let command = commands[pkgManager][type]
+
+  if (options.dev) {
+    command += ` ${commands[pkgManager].devOption}`
+  }
+
+  command += ` ${pkg}`
+
+  return command
 }
 
 export type CommandType = 'add'
+
+export interface CommandOptions {
+  dev?: boolean
+}
 
 export type PackageManager = (typeof pkgManagers)[number]
--- packages/starlight-package-managers/tests/unit/commands.test.ts
+++ packages/starlight-package-managers/tests/unit/commands.test.ts
@@ -3,14 +3,37 @@ import { expect, test } from 'vitest'
 import { getCommand, pkgManagers } from '../../pkg'
 
 test("should generate the 'add' command", () => {
-  expect(getCommands('add', 'typescript')).toEqual([
-    'npm i typescript',
-    'yarn add typescript',
-    'pnpm add typescript',
-    'bun add typescript',
-    'ni typescript',
+  expect(getCommands('add', 'astro', {})).toEqual([
+    'npm i astro',
+    'yarn add astro',
+    'pnpm add astro',
+    'bun add astro',
+    'ni astro',
   ])
 })
+
+test("should generate the 'add' command for multiple packages", () => {
+  expect(getCommands('add', '@astrojs/starlight astro', {})).toEqual([
+    'npm i @astrojs/starlight astro',
+    'yarn add @astrojs/starlight astro',
+    'pnpm add @astrojs/starlight astro',
+    'bun add @astrojs/starlight astro',
+    'ni @astrojs/starlight astro',
+  ])
+})
+
+test("should generate the 'add' command for a dev dependency", () => {
+  expect(getCommands('add', 'astro', { dev: true })).toEqual([
+    'npm i -D astro',
+    'yarn add -D astro',
+    'pnpm add -D astro',
+    'bun add -d astro',
+    'ni -D astro',
+  ])
+})
+
+// TODO(HiDeoo)
+test.todo("should use the dev dependency option only for the 'add' command")
 
 function getCommands(...args: OmitFirstParameter<typeof getCommand>) {
   return pkgManagers.map((pkgManager) => getCommand(pkgManager, ...args))
